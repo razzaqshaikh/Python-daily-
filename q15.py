a79415f9aa834425aafb5540c8c1c0a892171f5f
razzaqshaikh <142483292+razzaqshaikh@users.noreply.github.com>
2025-03-14 03:24:44 -0700

    Delete q15.py

--- q15.py
+++
@@ -1,9 +0,0 @@
-#Q5: Write a function to convert a given string into title case (capitalize the first letter of each word).
-
-
-def captalize_word(s):
- return (s.title())
-
-text = "razzaque shaikh i am good person"
-
-print("Title case: " , captalize_word(text))
