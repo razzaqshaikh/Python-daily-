58b64d202ebe7d1a354fcef51652aceaf5b0847a
razzaqshaikh <142483292+razzaqshaikh@users.noreply.github.com>
2025-03-14 03:24:06 -0700

    Delete q12.py

--- q12.py
+++
@@ -1,9 +0,0 @@
-#Q2: Write a function to count the number of vowels in a string.
-
-
-Name = "Shaikh RazzAque"
-
-vowels = "aeiouAEIOU"
-
-count = {i: Name.count(i) for i in vowels if i in Name}
-print(count)
