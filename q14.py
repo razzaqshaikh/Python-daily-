4a2eafa6ab4c0006876e47390fbdfd7c1a416ff4
razzaqshaikh <142483292+razzaqshaikh@users.noreply.github.com>
2025-03-14 03:24:32 -0700

    Delete q14.py

--- q14.py
+++
@@ -1,9 +0,0 @@
-#Q4: Write a function to count the number of words in a string.
-
-
-def count_words(s):
-    return len(s.split())
-
-
-text = "Hello how are you doing today?"
-print("Word count:", count_words(text))
